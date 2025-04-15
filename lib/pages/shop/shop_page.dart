9b1fa28f8f1c80a3a648a7a56ce8b38acd627289
Ismayil <ismayilov_ismayil_97@mail.ru>
2025-04-15 16:50:19 +0400

    start create sneaker card

--- lib/pages/shop/shop_page.dart
+++ lib/pages/shop/shop_page.dart
@@ -6,7 +6,52 @@ class ShopPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('Shop Page'),
+      child: Container(
+        width: 300,
+        height: 500,
+        decoration: BoxDecoration(
+          color: Colors.white70,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        clipBehavior: Clip.hardEdge,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Image.asset(
+              "images/ja.jpg",
+            ),
+            Text('Cool shoe'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    Text('Price'),
+                    Text('Size'),
+                  ],
+                ),
+                SizedBox(
+                  width: 50,
+                  height: 50,
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.add),
+                    style: ElevatedButton.styleFrom(
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(12),
+                        ),
+                      ),
+                      backgroundColor: Colors.grey.shade900,
+                      foregroundColor: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
